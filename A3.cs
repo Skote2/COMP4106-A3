9d0dc3fc2fbc6f9796aae310545be51b12bf9319
David Zilio <davidzilio@WINDOWS2.SCS.CARLETON.CA>
2019-04-08 17:28:09 -0400

    Initial setup of repository

---
+++ A3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COMP4106_A3
+{
+    class A3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
